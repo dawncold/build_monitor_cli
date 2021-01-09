3c82b93f7595e1e8f52046374b26cd7dbb7d15d1
Zhen Tian <zhen.tian@thoughtworks.com>
2021-01-09 21:35:20 +0800

    refactor: entrypoint cli; setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name = "build_monitor_cli",
     version = "1.0.0",
@@ -8,11 +11,19 @@ setup(
     description = 'Azure devops builds commandline monitor',
     license = "BSD",
     keywords = "azure devops",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['an_example_pypi_project', 'tests'],
-    long_description=read('README'),
+    url = 'https://github.com/dawncold/build_monitor_cli',
+    packages=['build_monitor_cli'],
+    long_description = 'Azure devops builds commandline monitor',
+    entry_points = {
+        'console_scripts': [
+            'bmonitor = build_monitor_cli:cli'
+        ]
+    },
+    install_requires=requirements,
+    python_requires='>=3.5',
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
+        'Environment :: Console',
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
     ],
